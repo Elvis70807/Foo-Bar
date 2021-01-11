4fd46613e9ecda489b050557ff941c379620bff7
Elvis Conte <elvisjconte@gmail.com>
2021-01-11 11:46:51 -0700

    Initial Commit

---
+++ FooBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FooBar
+//
+//  Created by Elvis Conte on 1/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
